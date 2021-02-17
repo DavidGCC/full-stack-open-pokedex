b596be7251e9cd7883649ee0bbd7635a00e91e81
DavidG <datog309@gmail.com>
2021-02-17 23:59:24 +0400

    e2e test

---
+++ cypress/integration/e2e.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+describe('Pokedex', () => {
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
